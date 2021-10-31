1babfad1af2d89a6fa1e67fb053fb396d5ce9953
guzenko_am <guzenko_am@nlmk.com>
2021-10-31 16:02:33 +0300

    feat(master): init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Check</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
